5e3d5bc3ff1b6865b3c61baaa11912517440db27
T9 <63940763+j3ss0109@users.noreply.github.com>
2021-02-20 10:43:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menulistclick(){
+    var list=document.getElementById("list");
+    if(list.style.display=="none"){
+        list.style.display="block";
+    }else{
+        list.style.display="none";
+    }
+}
