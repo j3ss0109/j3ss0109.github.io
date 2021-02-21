e37f4ad8d1c27bb411f522959714e9b29a10b7d5
j3ss0109 <j3ss01s09@gmail.com>
2021-02-21 10:51:22 +0800

    1

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ function menulistclick(){
     var list=document.getElementById("list");
     if(list.style.display=="none"){
         list.style.display="block";
+        list.style.animationName="list-on";
+        list.style.animationDuration="0.2s";
+        
     }else{
-        list.style.display="none";
+        list.style.animationName="list-off";
+        list.style.animationDuration="0.2s";
+        setTimeout(function(){
+            list.style.display="none";
+        },100);
     }
 }
 
